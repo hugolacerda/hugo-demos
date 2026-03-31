52f1470dab0d940f3f23ba7bdfc39809a3e37a91
hugolacerda <hugolacerda20@gmail.com>
2026-03-30 20:13:41 -0400

    SBV service pages: distinct hero treatments, placeholder images

---
+++ SBV_original/index.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- TODO: Set the document title to the name of your application -->
+    <title>SBV New Homepage Wireframe</title>
+    <meta name="description" content="SBV New Home Page">
+    <meta name="author" content="Lovable" />
+
+    <!-- TODO: Update og:title to match your application name -->
+    
+    
+    <meta property="og:type" content="website" />
+    <meta property="og:image" content="https://pub-bb2e103a32db4e198524a2e9ed8f35b4.r2.dev/20d66f06-bed0-4b79-bc11-27b519a09357/id-preview-43273348--e76f3ac1-818e-4f4a-9e50-1994bee70d1b.lovable.app-1774391696922.png">
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@Lovable" />
+    <meta name="twitter:image" content="https://pub-bb2e103a32db4e198524a2e9ed8f35b4.r2.dev/20d66f06-bed0-4b79-bc11-27b519a09357/id-preview-43273348--e76f3ac1-818e-4f4a-9e50-1994bee70d1b.lovable.app-1774391696922.png">
+    
+  
+  
+  
+  <meta property="og:title" content="SBV New Homepage Wireframe">
+  <meta name="twitter:title" content="SBV New Homepage Wireframe">
+  <meta property="og:description" content="SBV New Home Page">
+  <meta name="twitter:description" content="SBV New Home Page">
+  <script type="module" crossorigin src="/assets/index-DYeCNuCj.js"></script>
+  <link rel="stylesheet" crossorigin href="/assets/index-BUhrSxsY.css">
+<script defer src="https://sbv-homepage-wireframe-hf.mecpartners.com/~flock.js" data-proxy-url="https://sbv-homepage-wireframe-hf.mecpartners.com/~api/analytics"></script></head>
+
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
